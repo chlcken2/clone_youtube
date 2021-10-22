c78603c25c14cdf1ce77fbaae28f640dcfaac0fb
chl.cken <ckdgus988@naver.com>
2021-10-22 14:02:21 +0900

    make youtube web cleare

--- src/app.jsx
+++ src/app.jsx
@@ -1,25 +1,48 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import styles from "./app.module.css";
 import VideoList from "./components/video_list/video_list";
 import SearchHeader from "./components/search_header/search_header";
+import VideoDetail from "./components/video_detail/video_detail";
 
 function App({ youtube }) {
   const [videos, setVideos] = useState([]);
-  const search = (query) => {
-    youtube
-      .search(query) //
-      .then((videos) => setVideos(videos));
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const selectVideo = (video) => {
+    setSelectedVideo(video);
   };
+  const search = useCallback(
+    (query) => {
+      setSelectedVideo(null);
+      youtube
+        .search(query) //
+        .then((videos) => {
+          setVideos(videos);
+        });
+    },
+    [youtube]
+  );
 
   useEffect(() => {
     youtube
       .mostPopular() //
       .then((videos) => setVideos(videos));
-  }, []);
+  }, [youtube]);
   return (
     <div className={styles.app}>
       <SearchHeader onSearch={search} />
-      <VideoList videos={videos} />
+      <section className={styles.content}>
+        <div className={styles.detail}>
+          {selectedVideo && <VideoDetail video={selectedVideo} />}
+        </div>
+        <div className={styles.list}>
+          <VideoList
+            videos={videos}
+            onVideoClick={selectVideo}
+            display={selectedVideo ? "list" : "grid"} //VideoList한테 어떻게 display props를 만들어서 전달
+          />
+        </div>
+      </section>
     </div>
   );
 }
--- src/components/video_item/video_item.jsx
+++ src/components/video_item/video_item.jsx
@@ -1,21 +1,29 @@
-import React from "react";
+import React, { memo } from "react";
 import styles from "./video_item.module.css";
 
 //video>item>snippet
-const VideoItem = ({ video: { snippet } }) => (
-  <li className={styles.container}>
-    <div className={styles.video}>
-      <img
-        className={styles.thumbnail}
-        src={snippet.thumbnails.medium.url}
-        alt="video thumbnail"
-      />
-      <div className={styles.metadata}>
-        <p className={styles.title}>{snippet.title}</p>
-        <p className={styles.channel}>{snippet.channelTitle}</p>
-      </div>
-    </div>
-  </li>
+const VideoItem = memo(
+  ({ video, video: { snippet }, onVideoClick, display }) => {
+    const displayType = display === "list" ? styles.list : styles.grid; //displayType에 display타입이 리스트면 stylelist를쓰고 아니면 grid를 써
+    return (
+      <li
+        className={`${styles.container} ${displayType}`}
+        onClick={() => onVideoClick(video)}
+      >
+        <div className={styles.video}>
+          <img
+            className={styles.thumbnail}
+            src={snippet.thumbnails.medium.url}
+            alt="video thumbnail"
+          />
+          <div className={styles.metadata}>
+            <p className={styles.title}>{snippet.title}</p>
+            <p className={styles.channel}>{snippet.channelTitle}</p>
+          </div>
+        </div>
+      </li>
+    );
+  }
 );
 
 export default VideoItem;
